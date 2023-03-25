b88affb5a320f3447d88dd2f77a0d17e1adf50d4
tin yau leung <leungtinyau27m5@gmail.com>
2023-03-25 12:28:44 +0800

    refactor: rename i18n files

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserRegisterDTO } from './dto/in/user-register.dto';
+import { UserService } from './user.service';
 
 @Controller('api/user/v1')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() userRegisterDTO: UserRegisterDTO) {
+    return this.userService.register(userRegisterDTO);
+  }
+}
