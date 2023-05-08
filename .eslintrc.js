a8b205380b0013675ac34ab1b7c37d0caffbd3b2
betullkoyuncu <117205036+betullkoyuncu@users.noreply.github.com>
2023-05-08 14:11:11 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,13 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-        "node/no-missing-import": ["error", {
-        "allowModules": [],
-        "resolvePaths": ["/path/to/a/modules/directory"],
-        "tryExtensions": [".js", ".json", ".node"]
-    }]
-  },
-};
-
-
+    "settings": {
+        "node": {
+            "allowModules": ["electron"],
+            "resolvePaths": [__dirname],
+            "tryExtensions": [".js", ".json", ".node"]
+        }
+    },
+    "rules": {
+        "node/no-missing-import": "error"
+    }
+}
 
